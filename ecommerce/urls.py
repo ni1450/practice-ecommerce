841b2db81df8fdba9d9c25c7892bd787c62f1204
ni1450 <ni1450807@gmail.com>
2018-11-22 21:16:14 +0800

    ecommerce second commit

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
 
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 
 from .views import home_page, contact_page, login_page, register_page
 
@@ -28,6 +29,10 @@ urlpatterns = [
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
     url(r'^register/$', register_page),
+    url(r'^products/$', ProductListView.as_view()),
+    url(r'^products-fbv/$', product_list_view),
+    url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
 ]
 
 if settings.DEBUG:

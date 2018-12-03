4859dda99a536cb4f3944217aa9e225e6901d6ad
ni1450 <ni1450807@gmail.com>
2018-12-03 22:35:49 +0800

    ecommerce 7th commit

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
 from django.urls import reverse
-from .utils import unique_slug_generator
+from ecommerce.utils import unique_slug_generator
 
 # Create your models here.
 
@@ -79,6 +79,10 @@ class Product(models.Model):
         return self.title
 
     def __unicode__(self):
+        return self.title
+
+    @property
+    def name(self):
         return self.title
 
 

9b8f38c1754c436bc0dd63cec1ea8a6b936065aa
ni1450 <ni1450807@gmail.com>
2018-12-18 21:33:21 +0800

    ecommerce 13th commit

--- billing/views.py
+++ billing/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import stripe
 
-# Create your views here.
+stripe.api_key = "sk_test_sfV52hBYwCO5Jtvu9NRtJTlt"
+STRIPE_PUB_KEY = 'pk_test_Jqmc6mRzEWuEynf8ObY5J90c'
+
+
+def payment_method_view(request):
+    if request.method == "POST":
+        print(request.POST)
+    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY})
--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
+from analytics.mixin import ObjectViewedMixin
 from carts.models import Cart
 
 from .models import Product
@@ -15,7 +16,7 @@ class ProductFeaturedListView(ListView):
         return Product.objects.all().featured()
 
 
-class ProductFeaturedDetailView(DetailView):
+class ProductFeaturedDetailView(ObjectViewedMixin, DetailView):
     queryset = Product.objects.all().featured()
     template_name = "products/featured-detail.html"
 
@@ -53,7 +54,7 @@ def product_list_view(request):
     return render(request, "products/list.html", context)
 
 
-class ProductDetailSlugView(DetailView):
+class ProductDetailSlugView(ObjectViewedMixin, DetailView):
     queryset = Product.objects.all()
     template_name = "products/detail.html"
 
@@ -77,10 +78,12 @@ class ProductDetailSlugView(DetailView):
             instance = qs.first()
         except:
             raise Http404("????")
+        # object_viewed_signal.send(
+        #     instance.__class__, instance=instance, request=request)
         return instance
 
 
-class ProductDetailView(DetailView):
+class ProductDetailView(ObjectViewedMixin, DetailView):
     # queryset = Product.objects.all()
     template_name = "products/detail.html"
 
